6a64771ffd03a2098ac4add2908d59448c5dd3bd
Igor Nascimento <nigor6803@gmail.com>
2024-05-17 21:29:03 -0300

    produto usado

---
+++ src/entidade/ProdutoUsado.java
@@ -0,0 +1,33 @@
+package entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ProdutoUsado extends Produto {
+	
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Date data;
+	
+	public ProdutoUsado() {
+		super();
+	}
+	
+	public ProdutoUsado(String nome,Double preco, Date data) {
+		super(nome,preco);
+		this.data = data;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+	
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	@Override
+	public String etiquetaDePreco() {
+		return super.etiquetaDePreco()+"Data de fabricação"+sdf.format(data);
+	}
+}
